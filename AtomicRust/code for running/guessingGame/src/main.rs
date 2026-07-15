f1d816ed2b289644dadabee0e37329b5068fcf45
0xClumzZy <sustee848@gmail.com>
2026-07-15 13:47:03 +0200

    vault backup: 2026-07-15 13:47:03

--- AtomicRust/code for running/guessingGame/src/main.rs
+++ AtomicRust/code for running/guessingGame/src/main.rs
@@ -1,13 +1,19 @@
 use std::io;
+use rand::Rng;
 
 fn main(){
 	println!("Guess.......");
-	println!("input your guess: ");
+	let secret = rand::thread_rng().gen_range(1..=100);
+	println!("Input yo guess: ");
 
 	let mut guess = String::new();
 
 	io::stdin()
 		.read_line(&mut guess)
-		.expect("failed to readline");
-	println!("your guess is: {guess}");
+		.expect("failed to readd the line");
+	println!("yo dumb ahh guessed: {guess}");
+	println!("this the secret: {secret}");
+
+
+
 }
