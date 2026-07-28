dd8ccdab94dc8798ad4097150ca6fcc00c79040b
0xClumzZy <sustee848@gmail.com>
2026-07-28 22:06:12 +0200

    vault backup: 2026-07-28 22:06:12

--- aox/cCode/functions.c
+++ aox/cCode/functions.c
@@ -8,9 +8,15 @@ void song(char name[], int age){
     printf("happy birthday to %s\n", name);
     printf("how old are you now: %d ", age);
     printf("how old are you now: %d ", age);
-} 
+}
 int main(){
     char name[50] = "";
     int age = 0;
+
+    printf("what is yo name? \nENTER HERE=> ");
+    fgets(name, sizeof(name), stdin );
+    name[strlen(name) - 1] = '\0';
+
+    pr
     return 0;
 }
