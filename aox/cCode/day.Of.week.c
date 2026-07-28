4a36ef295b18582cbfe4c3ae63f5db0fb27f28bf
0xClumzZy <sustee848@gmail.com>
2026-07-28 19:14:07 +0200

    vault backup: 2026-07-28 19:14:07

---
+++ aox/cCode/day.Of.week.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int day = 0;
+    switch(day){
+        case 1:
+            printf("It is monday")
+        case 1:
+            printf("It is monday")
+        case 1:
+            printf("It is monday")
+        case 1:
+            printf("It is monday")
+        
+    }
+    
+
+    
+    return 0;
+}
