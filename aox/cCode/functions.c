cfce76be96036da4c0817d69f8e3462ed1565624
0xClumzZy <sustee848@gmail.com>
2026-07-28 22:02:01 +0200

    vault backup: 2026-07-28 22:02:01

--- aox/cCode/functions.c
+++ aox/cCode/functions.c
@@ -3,5 +3,10 @@
 
 
 void song(char name[], int age){
+    printf("happy birthday to you");
+    printf("happy birthday to you");
+    printf("happy birthday to %s\n", name);
+    printf( ");
+    printf("happy birthday to you");
     
 } 
