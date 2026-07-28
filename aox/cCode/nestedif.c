8ae601e22d75caa5536c5bbb94db002ceadfa9a4
0xClumzZy <sustee848@gmail.com>
2026-07-28 19:59:20 +0200

    vault backup: 2026-07-28 19:59:20

--- aox/cCode/nestedif.c
+++ aox/cCode/nestedif.c
@@ -8,11 +8,16 @@ int main() {
     bool isSenior = false;
 
     //student = 10% discount
-    //student = 1% discount
-    //student = 10% discount
+    //senior = 20% discount
+    //student + senior = 30 discount
     
     if(isStudent){
-        printf("Yo ")
+        printf("You get a 10% discount\n ");
+        price *=0.9;
+        printf("pay P%.1f", price);
+        
+
+        
     }
    return 0; 
 }
