ae674cb9ec2bf108ce11f6e846b7fd5d2b4f52f1
0xClumzZy <sustee848@gmail.com>
2026-07-30 01:32:39 +0200

    vault backup: 2026-07-30 01:32:39

--- aox/cCode/banking.c
+++ aox/cCode/banking.c
@@ -17,8 +17,18 @@ int main(){
         printf("2.Deposit\n");
         printf("3.Check balance\n");
         printf("4.exit\n");
-
-        
+        //rotate
+        scanf("%d\n", &choice);
+        switch(choice){
+            case 1:
+                withdraw(balance);
+                break;
+            case 2:
+                deposit();
+                break;
+            case 3;
+                
+        }
     }while(choice !=4)
     
     
