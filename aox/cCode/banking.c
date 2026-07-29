e8673a2f52a020b6661cbe52141dec61629b5ed0
0xClumzZy <sustee848@gmail.com>
2026-07-30 01:30:33 +0200

    vault backup: 2026-07-30 01:30:33

--- aox/cCode/banking.c
+++ aox/cCode/banking.c
@@ -10,14 +10,18 @@ int main(){
     int choice = 0;
     float balance = 0.0f;
 
-    printf("**WELCOME TO CLUMZZY BANKING SERVICES**\n");
-    printf("\nSELECT OPTION\n");
-    printf("1.Withdraw\n");
-    printf("2.Deposit\n");
-    printf("3.Check balance\n");
-    printf("4.exit\n");
+    do{
+        printf("**WELCOME TO CLUMZZY BANKING SERVICES**\n");
+        printf("\nSELECT OPTION\n");
+        printf("1.Withdraw\n");
+        printf("2.Deposit\n");
+        printf("3.Check balance\n");
+        printf("4.exit\n");
 
-    scanf("%d\n", );
+        
+    }while(choice !=4)
+    
+    
     return 0;
 }
 
