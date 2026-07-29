a49cb98f23d7c34edca3322ece5edfc415d8adc9
0xClumzZy <sustee848@gmail.com>
2026-07-30 01:16:02 +0200

    vault backup: 2026-07-30 01:16:01

--- aox/cCode/banking.c
+++ aox/cCode/banking.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-#include <unistd.h
+#include <unistd.h> 
+#include <stdbool.h>
+
+void checkBalance(float balance);
+float withdraw(float balance);
+float deposit();
+
+int main(float balan){
+    
+}
