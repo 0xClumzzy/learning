cf3d83f19c858b4fdaa50c0465ea66d4af538b7e
0xClumzzy <sustee848@gmail.com>
2026-09-10 23:11:39 +0200

    vault backup: 2026-09-10 23:11:38

--- AtomicRust/code for running/variables/src/main.rs
+++ AtomicRust/code for running/variables/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    let mut x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("Inner scope is: {x}");
+    }
+    println!("the outer scope value is: {x}");
+
 }
