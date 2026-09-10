42de86f0bd63fc4b0f3a51625549a535c7f3eed3
0xClumzzy <sustee848@gmail.com>
2026-09-11 01:18:26 +0200

    vault backup: 2026-09-11 01:18:26

---
+++ AtomicRust/code for running/arrays/target/debug/arrays.d
@@ -0,0 +1,1 @@
+/home/lemonx/learning/AtomicRust/code\ for\ running/arrays/target/debug/arrays: /home/lemonx/learning/AtomicRust/code\ for\ running/arrays/src/main.rs
---
+++ AtomicRust/code for running/arrays/target/debug/deps/arrays-2bb55e52adfbb44b.d
@@ -0,0 +1,5 @@
+/home/lemonx/learning/AtomicRust/code for running/arrays/target/debug/deps/arrays-2bb55e52adfbb44b.d: src/main.rs
+
+/home/lemonx/learning/AtomicRust/code for running/arrays/target/debug/deps/arrays-2bb55e52adfbb44b: src/main.rs
+
+src/main.rs:
