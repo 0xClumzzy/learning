fb769fb34615410273b03a8d3ed934882620a8e0
0xClumzzy <sustee848@gmail.com>
2026-09-11 01:16:14 +0200

    vault backup: 2026-09-11 01:16:13

--- AtomicRust/code for running/arrays/src/main.rs
+++ AtomicRust/code for running/arrays/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let a = [1,2,3,4,5];
+    println!("Enter your index: ");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+    let element = a[index];
+
+    println!("The value at index:{index} is: {element}");
 }
+
